9bd17c82921ab6bffa430f460c4dbbc5e585ed4d
Oisin Cassidy <oisin.c@hotmail.co.uk>
2019-01-14 13:16:22 +0000

    Added files

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-root',
+  template: `<div> 
+    <h1>{{pageTitle}}</h1> 
+    <pl-product></pl-product>
+  </div>`
+})
+
+export class AppComponent {
+  pageTitle: string = 'Product Management';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list-component';
+import { ConvertToSpacesPipe } from './Shared/convert-to-spaces.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
